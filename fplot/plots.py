bb08b55efa6a1cf204ac3d6e03b296467718a55c
Joe Yesselman <jyesselm@unl.edu>
2020-04-15 11:25:18 -0500

    finished bar plots working on group bar plots

--- fplot/plots.py
+++ fplot/plots.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import numpy as np
+import math
+import matplotlib.textpath as textpath
 from matplotlib import gridspec
 from collections import namedtuple
 
@@ -10,82 +12,182 @@ sns.set_style("white")
 sns.set_context("talk")
 sns.set_style("ticks")
 
-class PlotInfo(object):
-    def __init__(self):
-        self.x_labels = None
-        self.x_label = None
-        self.x_ticks = None
-        self.rotate_x_labels = None
 
-        self.y_label = None
-        self.y_labels = None
-        self.y_ticks = None
-        self.rotate_y_labels = None
+def __get_plot_kwargs(kwargs):
+    valid = "title,color,width".split(",")
+
+    plot_kwargs = {}
+    for k, v in kwargs.items():
+        if k in valid:
+            plot_kwargs[k] = v
+    return plot_kwargs
 
 
-def __get_plot_info(plot_info):
-    pi = PlotInfo()
+def __get_avg_char_length():
+    all_chars = "abcdefghijklmnopqrstuvwxyz0123456790"
+    avg_len = {}
+    for i in np.arange(0.2, 14, 0.1):
+        i = round(i, 1)
+        t = textpath.TextPath((0, 0), all_chars, size=i)
+        contents = t.get_extents()
+        act_width = contents.x1 - contents.x0
+        avg_len[i] = act_width / len(all_chars)
+    return avg_len
 
-    names = [a for a in dir(pi) if not a.startswith('__')]
 
-    for n in names:
-        if n in plot_info:
-            pi.__setattr__(n, plot_info[n])
+def __find_ideal_font_size(line, size):
+    best_size = 12.0
+    avg_lengths = __get_avg_char_length()
+    act_width = avg_lengths[best_size]*len(line)
+    while act_width > size:
+        best_size = round(best_size-0.1, 1)
+        act_width = avg_lengths[best_size] * len(line)
+        if best_size < 3.0:
+            break
+    return (best_size-0.1)
 
-    return pi
 
+def __get_fig_size_by_data(num_points, x_labels, cols):
+    #initial_figsize = [6.4, 4.8] # default matplotlib ratio
+    scale_size = num_points * cols * 0.14
+    if x_labels is None:
+        if scale_size < 6.4:
+            return (6.4, 4.8)
+        else:
+            return (scale_size, 4.8)
 
-def __render_x_labels(pi, ax):
-    if pi.x_labels:
-        ax.set_xticks(np.arange(len(pi.x_labels)))
-        ax.set_xticklabels(list(pi.x_labels))
     else:
-        ax.set_xticklabels([])
+        if scale_size < 6.4:
+            return (6.4, 4.8)
+        else:
+            return (scale_size, 4.8)
 
-
-def __render_y_labels(pi, ax):
-    if pi.x_labels:
-        ax.set_yticks(np.arange(len(pi.y_labels)))
-        ax.set_yticklabels(list(pi.y_labels))
-    else:
-        ax.set_yticklabels([])
 
 
 #########################################################################################
 # different plot types
 #########################################################################################
 
+def __bar_plot(x, y, ax, **kwargs):
+    ax.bar(x, y, **kwargs)
+
 #########################################################################################
 # BAR PLOTS
 #########################################################################################
 
-def bar_plot(df, y, x=None, ax=None, fig=None, **kwargs):
-    pi = __get_plot_info(kwargs)
+def bar_plot(bars, x_labels=None, ax=None, fig=None, **kwargs):
+    width = 1
+    spacing = width * 1.10
+    ax_is_none = 0
+    x_data = []
 
-    if ax is None and len(df) < 150:
-        fig, ax = plt.subplots(1)
-    elif ax is None:
-        print("WARNING, bar plot requires scaling")
-        fig, ax = plt.subplots(figsize=(len(df)*0.10, 3))
+    fig_size = __get_fig_size_by_data(len(bars), x_labels, 1)
 
-    y_data = list(df[y])
-    x_data = np.arange(0, len(df))
-    #if x is not None:
-    #    x_data = list(df[x])
+    if ax is None:
+        ax_is_none = 1
+        fig, ax = plt.subplots(figsize=fig_size, dpi=300)
+        x_data = np.linspace(0, len(bars)*width*1.10, len(bars))
+    else:
+        x_lim = ax.get_xlim()
+        x_data = np.linspace(x_lim[0], x_lim[1], len(bars))
+        width = (x_data[0]-x_data[1])*0.90
 
-    ax.bar(x_data, y_data)
+    plot_kwargs = __get_plot_kwargs(kwargs)
+    plot_kwargs["width"] = width
 
-    #__render_x_labels(pi, ax)
+    # TODO should not hardcode this
+    plot_kwargs["linewidth"] = 0
 
-    fig.tight_layout()
+    if not "color" in plot_kwargs:
+        plot_kwargs["color"] = '#4c72b0'
+
+    __bar_plot(x_data, bars, ax, **plot_kwargs)
+
+    # for bar plots having default ticks make no sense, just remove them
+    if ax_is_none:
+        # TODO need some more complex scaling for small number of bars with default figsize
+        ax.set_xlim([x_data[0]-width*1.5, x_data[-1]+width*1.5])
+
+    wont_fit = 0
+    if x_labels is not None:
+        label_length = "".join(x_labels)
+        max_length = (fig_size[0] * 0.9) * 60  # 35 is a random scale
+        ideal_size_horizontal = __find_ideal_font_size(label_length, max_length)
+
+        if fig_size[0] < 6.5 and ideal_size_horizontal > 3:
+            ideal_size_horizontal = 14
+        elif fig_size[0] < 6.5 and len(bars) < 12:
+            ideal_size_horizontal = 12
+            wont_fit = 1
+
+        if ideal_size_horizontal < 3.0:
+            wont_fit = 1
+            ideal_size_horizontal = 6.0
+        if ideal_size_horizontal < 3.0:
+            ideal_size_horizontal = 3.0
+            print("WARNING text is going to be squished")
+        ax.set_xticks(x_data)
+
+        if wont_fit:
+            ax.set_xticklabels(x_labels, fontdict={'fontsize': 6}, rotation=90)
+        else:
+            ax.set_xticklabels(x_labels, fontdict={'fontsize': ideal_size_horizontal})
+    else:
+        ax.set_xticks([])
+
+
+    if ax_is_none:
+        ax.set_xlim([x_data[0]-width*1.5, x_data[-1]+width*1.5])
+        fig.tight_layout()
+
+    return fig, ax
+
+
+def group_bar_plot(df, cols, x_labels, col_labels=None, ax=None, fig=None, **kwargs):
+    fig_size = __get_fig_size_by_data(len(df), x_labels, 1)
+    width = 1
+    ax_is_none = 0
+
+    if ax is None:
+        ax_is_none = 1
+        fig, ax = plt.subplots(figsize=fig_size, dpi=300)
+        x_data = np.linspace(0, width*len(df)*(len(cols)+1), len(df)*(len(cols)+1))
+    else:
+        x_lim = ax.get_xlim()
+        x_data = np.linspace(x_lim[0], x_lim[1], len(df))
+        width = (x_data[0] - x_data[1]) * 0.90
+
+    bars = []
+    for c in cols:
+        bars.append(list(df[c]))
+
+    rs = [[] for x in range(len(cols)+1)]
+    count = 0
+    for i in range(len(x_data)):
+        rs[count].append(x_data[i])
+        count += 1
+        if count > len(cols):
+            count = 0
+
+    if col_labels is None:
+        col_labels = cols
+
+    for i in range(len(bars)):
+        ax.bar(rs[i], bars[i], width=width, linewidth=0, label=col_labels[i])
+
+    #ax.set_xticks([r + width for r in range(len(bars[0]))])
+    #ax.set_xticklabels(x_labels)
+    ax.set_xticks([])
+
+    if ax_is_none:
+        #ax.set_xlim([x_data[0] - width * 1.5, x_data[-1] + width * 1.5])
+        fig.tight_layout()
 
     return fig, ax
 
 
 def main():
-    d = {"x_labels" : [0, 1, 2]}
-    pi = __get_plot_info(d)
-    print(pi.x_labels)
+    pass
 
 if __name__ == "__main__":
 
--- tests/plots_tests.py
+++ tests/plots_tests.py
@@ -3,6 +3,7 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 import unittest
+import uuid
 
 from fplot.plots import *
 
@@ -13,19 +14,111 @@ sns.set_context("talk")
 class Unittest(unittest.TestCase):
 
     def test_bar_plot(self):
-        df = sns.load_dataset("anscombe")
-        fig, ax = bar_plot(df, "x")
-        fig.savefig("outputted_plots/bar_plot_1.png", dpi=100)
-        plt.clf()
+        self.skipTest("all works for now")
 
-        df = sns.load_dataset("tips")
-        fig, ax = bar_plot(df, "total_bill")
-        fig.savefig("outputted_plots/bar_plot_2.png", dpi=100)
+        # basic test
+        df_a = sns.load_dataset("anscombe")
+        df_t = sns.load_dataset("tips")
+
+        fig, ax = bar_plot(df_a["x"])
+        fig.savefig("outputted_plots/bar_plot_1.pdf")
+
+        # smaller number of bars
+        fig, ax = bar_plot(df_a["x"][:4])
+        fig.savefig("outputted_plots/bar_plot_2.pdf")
+
+        # larger number of bars
+        fig, ax = bar_plot(df_t["total_bill"])
+        fig.savefig("outputted_plots/bar_plot_3.pdf")
+
+        pal = sns.color_palette('deep')
+
+        # test specifying axis and figure instead of allowing the function to make it
+        fig, axs = plt.subplots(3)
+        bar_plot(df_a["x"], ax=axs[0], fig=fig, color=pal[0])
+        bar_plot(df_a["x"][:4], ax=axs[1], fig=fig, color=pal[1])
+        bar_plot(df_t["total_bill"], ax=axs[2], fig=fig, color=pal[2])
+        fig.savefig("outputted_plots/bar_plot_4.pdf")
+
+    def test_bar_plot_labels(self):
+        self.skipTest("all works for now")
+
+        df_a = sns.load_dataset("anscombe")
+        df_t = sns.load_dataset("tips")
+
+        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        labels = []
+        c = 0
+        for i in range(0, 100):
+            labels.append(chars[c] * 10)
+            c += 1
+            if c == len(chars):
+                c = 0
+
+        # add labels
+        fig, ax = bar_plot(df_t["total_bill"],
+                           x_labels="A" * len(df_t["total_bill"]))
+        fig.savefig("outputted_plots/bar_plot_5.pdf")
+
+        long_labels = [str(uuid.uuid1()) for x in range(0, len(df_t))]
+        fig, ax = bar_plot(df_t["total_bill"][0:10],
+                           x_labels=long_labels[0:10])
+        fig.savefig("outputted_plots/bar_plot_6.pdf")
+
+
+        fig, ax = bar_plot(df_t["total_bill"][0:2], x_labels=labels[0:2])
+        fig.savefig("outputted_plots/bar_plot_7.pdf")
+
+        fig, ax = bar_plot(df_t["total_bill"][0:50],
+                           x_labels="A" * len(df_t["total_bill"][0:50]))
+        fig.savefig("outputted_plots/bar_plot_8.pdf")
+
+        df = pd.concat([df_t, df_t])
+
+        fig, ax = bar_plot(df["total_bill"],
+                           x_labels="A" * len(df))
+        fig.savefig("outputted_plots/bar_plot_9.pdf")
+
+        pal = sns.color_palette('deep')
+
+        # test specifying axis and figure instead of allowing the function to make it
+        fig, axs = plt.subplots(3, figsize=(6.4, 10))
+        bar_plot(df_a["x"][0:3], ax=axs[0], fig=fig, color=pal[0], x_labels="A,B,C".split(","))
+        bar_plot(df_a["x"], ax=axs[1], fig=fig, color=pal[1], x_labels=labels[0:len(df_a)])
+        bar_plot(df_t["total_bill"], ax=axs[2], fig=fig, color=pal[2])
+        fig.tight_layout()
+        fig.savefig("outputted_plots/bar_plot_10.pdf")
+
+        #fig, ax = plt.subplots(1)
+        #print(fig.get_size_inches())
+        #exit()
+
+
+        #ax.bar(np.arange(0, 5), df_t["total_bill"][0:5])
+        #ax.set_xticks(np.arange(0, 5))
+        #ax.set_xticklabels(shorter_labels[0:5])
+        #fig.savefig("test.pdf")
+
+    def test_group_bar_plot(self):
+        df = sns.load_dataset("anscombe")
+        #df = df[0:10]
+        fig, ax = group_bar_plot(df, ["x", "y"], df["dataset"])
+        fig.legend()
+        fig.savefig("outputted_plots/group_bar_plot_1.pdf")
+
+        #df["z"] = df["x"]*0.25 + df["y"]
+
+
 
     def _test_really_big_bar_plot(self):
         df = pd.read_csv("../data/helical_variation_final.csv")
-        fig, ax = bar_plot(df, "dG")
-        fig.savefig("outputted_plots/bar_plot_3.png", dpi=50)
+        fig, ax = bar_plot(df["dG"])
+        fig.savefig("outputted_plots/bar_plot_6.pdf")
+
+        #df = df[:100]
+        #fig, ax = plt.subplots(1)
+        #g = sns.barplot(np.linspace(0, 10, len(df)), df["dG"], ax=ax)
+        #fig.savefig("outputted_plots/bar_plot_7.pdf")
 
 
 
